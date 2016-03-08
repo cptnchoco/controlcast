d0728553dcb8ad9565121a2f1e6571d3bf4259bd
dbkynd <db@dbkynd.com>
2016-03-08 14:44:33 -0700

    Added Gruntfile
    This builds our packaged application into a Windows installer with
    squirrel

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'create-windows-installer': {
+            ia32: {
+                appDirectory: './dist/ControlCast-win32-ia32',
+                outputDirectory: './dist/win',
+                authors: 'DBKynd',
+                exe: 'ControlCast.exe',
+                loadingGif: './build/loading.gif',
+                iconUrl: 'https://raw.githubusercontent.com/dbkynd/controlcast/master/images/icon.ico',
+                setupIcon: './images/icon.ico',
+                noMsi: true
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-electron-installer');
+
+    grunt.registerTask('default', 'create-windows-installer');
+};
