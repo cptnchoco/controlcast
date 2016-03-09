525202b75b96457ed5cb50dc7be29f68dd9760e6
DBKynd <db@dbkynd.com>
2016-03-08 21:10:35 -0700

    Added Code Signing Cert

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,16 @@ module.exports = function (grunt) {
         'create-windows-installer': {
             ia32: {
                 appDirectory: './dist/ControlCast-win32-ia32',
-                outputDirectory: './dist/win',
+                outputDirectory: './dist',
                 authors: 'DBKynd',
                 exe: 'ControlCast.exe',
                 loadingGif: './build/loading.gif',
                 iconUrl: 'https://raw.githubusercontent.com/dbkynd/controlcast/master/images/icon.ico',
                 setupIcon: './images/icon.ico',
-                noMsi: true
+                noMsi: true,
+                certificateFile: '../CodeSigningCert.pfx',
+                certificatePassword: require('../CodeSigningPassword.json').password,
+                remoteReleases: 'http://vps.dbkynd.com/controlcast/releases'
             }
         }
     });
