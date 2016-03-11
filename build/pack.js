0d377a388e9173e751df7b77428dea997eb31613
DBKynd <db@dbkynd.com>
2016-03-10 19:10:52 -0700

    Not using asar
    Packaging this up into an asar archive broke the hotkeys from being sent
    for some reason. Need to investigate.

--- build/pack.js
+++ build/pack.js
@@ -8,7 +8,7 @@ var pack_options = {
     arch: 'ia32',
     dir: '../',
     out: '../dist',
-    asar: true,
+    asar: false,
     overwrite: true,
     icon: '../images/icon.ico',
     ignore: '^/.idea|^/build|^/dist|^/node_modules/(electron-*|grunt|grunt-*|rmdir)',
